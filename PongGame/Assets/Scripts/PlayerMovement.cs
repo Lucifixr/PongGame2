69640f592cb0161db949e75bc58c87dcb8a44834
Lucifixr <113519242+Lucifixr@users.noreply.github.com>
2022-09-15 15:53:37 +0200

    Movement speed veranderd Rackets
    Heb scripts op de twee rackets gedaan en heb movement speed veranderd

---
+++ PongGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerNumber = 1;
+    public float MoveSpeed = 5.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxis("Player1"));
+        if (playerNumber == 1)
+        {
+            transform.Translate(new Vector3(0, Input.GetAxis("Player1") * MoveSpeed * Time.deltaTime, 0));
+
+        }
+
+    }
+}
+
+      
+        
---
+++ PongGame/Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+    public int playerNumber = 2;
+    public float MoveSpeed = 3.0f; 
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerNumber == 2)
+        {
+            transform.Translate(new Vector3(0, Input.GetAxis("Player2") * MoveSpeed * Time.deltaTime, 0));
+    
+    }   }
+}
